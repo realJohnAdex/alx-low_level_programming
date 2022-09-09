c0b36f884d95440826949a8e4f45642c1fc1f017
realJohnAdex <johnadex60@gmail.com>
2022-09-09 13:36:30 -0700

    Add task 1 file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: check if random number is pos, neg or zero
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int n;
+	int n_mod;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	n_mod = n % 10;
+	if (n_mod > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, n_mod);
+	} else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is zero\n", n, n_mod);
+	} else if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n_mod);
+	}
+
+	return (0);
+}
