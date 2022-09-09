8f19b3124f0ea38f0b0f89e651762625e6c76f40
realJohnAdex <johnadex60@gmail.com>
2022-09-09 14:36:09 -0700

    Add task 3 file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: display uppercase and lowercase of alphabet
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	char lower;
+	char upper;
+
+	lower = 'a';
+	upper = 'A';
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+        {
+                putchar(upper);
+                upper++;
+        }
+	putchar('\n');
+
+	return (0);
+}
