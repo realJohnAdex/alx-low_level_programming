1fa01e42f56cdc070aa87b723e94d2a24f95a34c
realJohnAdex <johnadex60@gmail.com>
2022-09-09 16:43:25 -0700

    Add task 10 file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: display double digits and ,
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	i = 0;
+	while (i < 100)
+	{
+		j = i % 10;
+		k = i / 10;
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+			if (i < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
