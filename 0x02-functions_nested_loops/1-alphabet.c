306fdd672fe220e48030d0f1079f134acd12e64b
realJohnAdex <johnadex60@gmail.com>
2022-09-13 12:29:11 -0700

    Add task 1 file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print all lowercase character
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
