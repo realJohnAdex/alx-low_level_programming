ece607f67742fd4fb458847dd7329ab90ad24172
realJohnAdex <johnadex60@gmail.com>
2022-09-15 20:27:48 -0700

    Add task 6 file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line- draws a straight line in the terminal.
+ * @n: integer input
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
