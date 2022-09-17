ed677f82d2f3d7f3c9749820e04658720c172004
realJohnAdex <johnadex60@gmail.com>
2022-09-16 21:59:20 -0700

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,42 +10,14 @@
  */
 void print_number(int n)
 {
-	int units, tens, hundreds, thousands;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 == 0)
+	if ((n / 10) > 0)
 	{
-		_putchar(n + 48);
+		print_number(n / 10);
 	}
-	else if (n / 100 == 0)
-	{
-		tens = n / 10;
-		units = n % 10;
-		_putchar(tens + 48);
-		_putchar(units + 48);
-	}
-	else if (n / 1000 == 0)
-	{
-		hundreds = n / 100;
-		tens = (n / 10) % 10;
-		units = n % 10;
-		_putchar(hundreds + 48);
-		_putchar(tens + 48);
-		_putchar(units + 48);
-	}
-	else if (n / 10000 == 0)
-	{
-		thousands = n / 1000;
-		hundreds = (n / 100) % 10;
-		tens = (n / 10) % 10;
-		units = n % 10;
-		_putchar(thousands + 48);
-		_putchar(hundreds + 48);
-		_putchar(tens + 48);
-		_putchar(units + 48);
-	}
+	_putchar((n % 10) + 48);
 }
