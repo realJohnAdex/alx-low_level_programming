bfcf1b300d132adc8cad8ef0780f803c94e591e1
realJohnAdex <johnadex60@gmail.com>
2022-09-22 13:51:42 -0700

    Add task 0 file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - dereferencing to change int value
+ * @n: Integer variable
+ *
+ * Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
