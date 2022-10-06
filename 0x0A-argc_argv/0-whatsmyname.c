0f5d18d7e520179e2bca1d4d03fdbbe94f3139cf
realJohnAdex <johnadex60@gmail.com>
2022-10-06 02:41:59 -0700

    Add task 0 file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	while(argc--)
+	{
+		printf("%s\n", * argv++);
+	}
+	return 0;
+}
