83661383aa5b24d174512f1aba558382209e964f
realJohnAdex <johnadex60@gmail.com>
2022-10-06 05:54:56 -0700

    Update task 4 file 1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,23 +10,32 @@
  */
 int main(int argc, char **argv)
 {
-	int sum, i;
+	int sum, i, falsesum;
 
 	sum = 0;
+	falsesum = 0;
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) > 0)
+			if (atoi(argv[i]) == 0)
+			{
+				falsesum ++;
+			} else if (atoi(argv[i]) > 0)
 			{
 				sum += atoi(argv[i]);
-			} else
+			}
+			if (sum && falsesum)
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		printf("%d\n", sum);
+		if (falsesum == (argc - 1))
+		{
+			printf("0\n");
+		} else
+			printf("%d\n", sum);
 	} else
 		printf("0\n");
 	return (0);
