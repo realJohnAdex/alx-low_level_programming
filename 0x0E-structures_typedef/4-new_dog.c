b8da4df00e5ca7700da71aab6271c6234fb5d302
realJohnAdex <johnadex60@gmail.com>
2022-10-10 15:18:35 -0700

    Add task 4 file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	
+
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
-	return new_dog;
+	return (new_dog);
 }
