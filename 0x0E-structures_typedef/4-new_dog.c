8515b0abe8495856da5435634ff9fa1ef5bb2438
realJohnAdex <johnadex60@gmail.com>
2022-10-10 15:11:45 -0700

    Add task 4 file

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - a function that creates a new struct dog
+ * @name: name var
+ * @age: age var
+ * @owner: owner var
+ *
+ * Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return new_dog;
+}
