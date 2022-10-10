d9823a7b9d8e3c249c11f9d71ad7762448490283
realJohnAdex <johnadex60@gmail.com>
2022-10-10 15:34:50 -0700

    Add task 5 file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees struct dog
+ * @d: dog_t sturct
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+		free(d);
+}
