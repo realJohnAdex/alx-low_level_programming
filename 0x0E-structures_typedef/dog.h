914160e022999169ee930319c84702a13b7fb493
realJohnAdex <johnadex60@gmail.com>
2022-10-10 07:22:10 -0700

    Add task 0 file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a pretty little dog
+ * @name: name
+ * @age: age
+ * @owner: owner of dog
+ *
+ * Description: what a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
