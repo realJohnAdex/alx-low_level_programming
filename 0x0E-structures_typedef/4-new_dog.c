64d9c2fa9a4d651cf6f9f6cb9d04e70bd27bbfc7
John Adex <johnadex60@gmail.com>
2022-10-11 09:41:25 +0100

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	
-	new_dog = (dog_t *) malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
