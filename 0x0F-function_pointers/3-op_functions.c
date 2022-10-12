01779573bdd6343cc3e2ebbd8d4fd38cc43a5367
realJohnAdex <johnadex60@gmail.com>
2022-10-12 16:07:15 -0700

    Add task 3 file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - sum
+ * @a: int
+ * @b: int
+ *
+ * Return:  returns the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: int
+ * @b: int
+ *
+ * Return:  returns difference sum of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product
+ * @a: int
+ * @b: int
+ *
+ * Return:  returns the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: int
+ * @b: int
+ *
+ * Return:  returns the result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder
+ * @a: int
+ * @b: int
+ *
+ * Return:  returns the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
