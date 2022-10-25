8866ab4fda5b7c17575c1c61a476e77215c701ea
realJohnAdex <johnadex60@gmail.com>
2022-10-25 01:11:38 -0700

    update task 2 file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,15 +17,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	if (n)
-	{
-		new->n = n;
-		new->next = *head;
-		*head = new;
-	} else
-	{
-		return (NULL);
-	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
 	return (new);
 }
