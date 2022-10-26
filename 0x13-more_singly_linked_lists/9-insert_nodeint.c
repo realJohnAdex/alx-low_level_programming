e0d5aa13ef60f19b6edf4b7e38a83ac0949cef90
realJohnAdex <johnadex60@gmail.com>
2022-10-26 04:30:10 -0700

    Update task 9 file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		count++;
-		if (count == (idx-1))
+		if (count == (idx - 1))
 		{
 			new->next = temp->next;
 			temp->next = new;
