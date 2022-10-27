98eb7f004999ec2988659604d9e3d893e8889c07
realJohnAdex <johnadex60@gmail.com>
2022-10-27 14:15:22 -0700

    Add task 1 file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: unsigned long int
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int lenght;
+	unsigned long int mask, temp;
+
+	lenght = 0;
+	mask = 1;
+	temp = n;
+	while (temp > 0)
+	{
+		lenght++;
+		temp >>= 1;
+	}
+	lenght--;
+	if (lenght > 0)
+	{
+		mask <<= lenght;
+	}
+	while (mask > 0)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+		mask >>= 1;
+	}
+}
+
