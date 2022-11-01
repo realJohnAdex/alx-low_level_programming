ef25519f58e56a790ef71c79978e223d8142517c
realJohnAdex <johnadex60@gmail.com>
2022-11-01 06:21:50 -0700

    Add task 3 file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,41 +17,37 @@
  */
 int cp_file(const char *file_from, const char *file_to)
 {
-	int file_from, file_to, text_len;
+	int fd_from, fd_to, close_fd_from, close_fd_to;
 	unsigned char buf[1024];
-	size_t bytesRead = 0;
-	ssize_t w_check;
+	ssize_t bytesRead = 0;
+	ssize_t w_check = 0;
 
-	/* write */
-	file_from = open(file_from, O_RDONLY);
-	file_to = creat(file_to, 0664);
+	fd_from = open(file_from, O_RDONLY);
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	if (file_from == -1)
+	if (fd_from == -1)
 		return (98);
-	if (file_to == -1)
+	if (fd_to == -1)
 		return (99);
-	if ((file_from >= 0) && (file_to >= 0))
+	if ((fd_from >= 0) && (fd_to >= 0))
 	{
-		while ((bytesRead = read(file_from, buf, sizeof(buf))) > 0)
+		while ((bytesRead = read(fd_from, buf, sizeof(buf))) > 0)
 		{
-			w_check = write(file_to, buf, bytesRead);
-			if (w_check == -1)
-				return (99);
+			w_check = write(fd_to, buf, bytesRead);
 			if (w_check != bytesRead)
 			{
-				bytesRead = -1;
+				return (99);
 			}
 		}
 	}
 
-	if (w_check == -1)
-	{
-		return (-1);
-	}
-
-	close(file_from);
-	close(file_to);
-	return (sz == 0) ? 0 : -1;
+	close_fd_from = close(fd_from);
+	if (close_fd_from != 0)
+		return (fd_from + 100);
+	close_fd_to = close(fd_to);
+	if (close_fd_to != 0)
+		return (fd_to + 100);
+	return (0);
 }
 
 /**
@@ -64,19 +60,34 @@ int cp_file(const char *file_from, const char *file_to)
  */
 int main(int ac, char **av)
 {
-	int res;
+	int check;
 
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
-        }
+	}
 	if (!av[1])
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-        cp_file(av[1], av[2]);
+	check = cp_file(av[1], av[2]);
+	if (check == 99)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	if (check == 98)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (check == 100)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", (check - 100));
+		exit(100);
+	}
 
-        return (0);
+	return (0);
 }
