2fc4a9c465a2950017dbfcea2dca04de40500775
realJohnAdex <johnadex60@gmail.com>
2022-10-31 17:05:00 -0700

    Add task 1 file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: string name of file
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, text_len;
+
+	if (!filename)
+		return (-1);
+	/* write */
+	/*fd = open(filename, O_CREAT|O_WRONLY|O_TRUNC);*/
+	fd = creat(filename, 0600);
+	if (fd == -1)
+		return (-1);
+
+
+	text_len = strlen(text_content);
+	if (text_content != NULL)
+	{
+		write(fd, text_content, text_len);
+	}
+
+	close(fd);
+
+	return (1);
+}
