8e8d5a75fc593700a7c479018047c9dba3c95fbc
realJohnAdex <johnadex60@gmail.com>
2022-10-31 17:24:48 -0700

    Update task 1 file 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,8 +23,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	/* write */
-	/*fd = open(filename, O_CREAT|O_WRONLY|O_TRUNC);*/
-	fd = creat(filename, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0600);
+	/* fd = creat(filename, 0600);*/
 	if (fd == -1)
 		return (-1);
 
