e772551f329dfe87e5e4d65d9d8d2d33c5a5c39e
realJohnAdex <johnadex60@gmail.com>
2022-11-22 19:52:59 -0800

    Add task 8 file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d, ",a[i]);
+		printf("%d, ", a[i]);
 	}
-	printf("%d",a[i]);
+	printf("%d", a[i]);
 	printf("\n");
 }
