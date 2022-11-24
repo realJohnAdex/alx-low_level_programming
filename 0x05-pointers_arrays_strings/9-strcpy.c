96c973115f50da2b42b4574701e876f3ddf890fc
realJohnAdex <johnadex60@gmail.com>
2022-11-24 13:10:52 -0800

    Add task 9 file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies str pointed to by src, to buffer pointed to by dest
+ * @dest: buffer to copy into
+ * @src: string to copy
+ *
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	size_t i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i + 1] = '\0';
+
+	return (dest);
+}
