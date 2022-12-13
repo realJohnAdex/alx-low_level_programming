b7a02977f52b569da1e6a9f9a32d683eba34c2b8
realJohnAdex <johnadex60@gmail.com>
2022-12-12 20:15:38 -0800

    add operations function and object file

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include "py.h"
+
+/**
+* add - adds two ints
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of a & b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtract two ints
+* @a: first integer
+* @b: second integer
+*
+* Return: subtract of a & b
+*/
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+* mul - multiply two ints
+* @a: first integer
+* @b: second integer
+*
+* Return: product of a & b
+*/
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+* div - divide two ints
+* @a: first integer
+* @b: second integer
+*
+* Return: quotient of a & b
+*/
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+* mod - adds two ints
+* @a: first integer
+* @b: second integer
+*
+* Return: modulus of a & b
+*/
+int mod(int a, int b)
+{
+        return (a % b);
+}
