1539cfc92996a93f0e56c90c406cba6b23d84fc1
realjohnadex <johnadex60@gmail.com>
2024-03-20 17:20:03 -0700

    handle irregular shape

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """Island perimeter function"""
+
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
-    land = 0
-    for i in grid:
-        for j in i:
-            if j == 1:
-                land += 1
-    return (land * 2 ) + 2
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # check left
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # check right
+                if j == len(grid[i]) or grid[i][j + 1] == 0:
+                    perimeter += 1
+                # check down
+                if i == len(grid) or grid[i + 1][j] == 0:
+                    perimeter += 1
+                # check up
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+    return perimeter
